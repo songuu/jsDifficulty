2c9b84254650fa754f0eb3e06ed1b8d974770edb
songuu <1101309860@qq.com>
2021-08-15 17:29:43 +0800

    [更新]

--- react/test/build/constant.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: songyu
- * @Date: 2021-08-11 20:35:26
- * @LastEditTime: 2021-08-11 20:40:44
- * @LastEditors: songyu
- * @Description: 
- * @FilePath: \美洽\test\build\constant.ts
- */
-export const OUTPUT_DIR = 'dist';
---
+++ webpack/webpack插件/myBannerPlugin.js
@@ -0,0 +1,43 @@
+/*
+ * @Author: songyu
+ * @Date: 2021-08-15 16:58:28
+ * @LastEditTime: 2021-08-15 16:59:09
+ * @LastEditors: songyu
+ * @Description:
+ * @FilePath: \项目文件\jsDifficulty\webpack\webpack插件\myBannerPlugin.js
+ */
+const { Dependency, Template } = require("webpack");
+
+class DemoDependency extends Dependency {
+  constructor() {
+    super();
+  }
+}
+
+DemoDependency.Template = class DemoDependencyTemplate extends Template {
+  apply(dependency, source) {
+    const today = new Date().toLocaleDateString();
+    source.insert(
+      0,
+      `/* hello: world */
+/* Date: ${today} */
+`
+    );
+  }
+};
+
+module.exports = class DemoPlugin {
+  apply(compiler) {
+    compiler.hooks.thisCompilation.tap("DemoPlugin", (compilation) => {
+      // 调用 dependencyTemplates ，注册 Dependency 到 Template 的映射
+      compilation.dependencyTemplates.set(
+        DemoDependency,
+        new DemoDependency.Template()
+      );
+      compilation.hooks.succeedModule.tap("DemoPlugin", (module) => {
+        // 模块构建完毕后，插入 DemoDependency 对象
+        module.addDependency(new DemoDependency());
+      });
+    });
+  }
+};
